55aeb473490995fc94d2925630dec85730e851f5
Hustek <lukaszeksjz@gmail.com>
2017-01-15 15:04:44 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+
+  $("#idForm").submit(function(e) {
+
+    var url = "mailer.php"; // the script where you handle the form input.
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $("#idForm").serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               alert(data); // show response from the php script.
+           }
+         });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});
+
+
   $("#navbar ul li a[href^='#']").on('click', function(e) {
      // prevent default anchor click behavior
      e.preventDefault();
